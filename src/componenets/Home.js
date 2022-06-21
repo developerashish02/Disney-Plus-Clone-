97cb2cf7367717b955fdb89aa7308ab1ccdb0e62
Ashish_Amar_Gaikwad <developerashish02@gmail.com>
2022-06-21 13:39:13 +0530

    ADD Viewers

--- src/componenets/Home.js
+++ src/componenets/Home.js
@@ -4,10 +4,12 @@ import ImgSlider from "./ImgSlider";
 import Viewers from "./Viewers";
 
 const Home = () => {
-    return <Container>
-        <ImgSlider />
-        <Viewers />
-    </Container>;
+    return (
+        <Container>
+            <ImgSlider />
+            <Viewers />
+        </Container>
+    );
 };
 
 export default Home;
@@ -16,17 +18,17 @@ const Container = styled.main`
 	min-height: calc(100vh - 70px);
 	padding: 0 calc(3.5vw + 5px);
 	position: relative;
-    overflow-x: hidden;
+	overflow-x: hidden;
 
 	&:before {
 		background: url("/Disney Plus/images/home-background.png") center center /
 			cover no-repeat fixed;
-            content: "";
-            position: absolute;
-            top: 0;
-            left: 0;
-            right: 0;
-            bottom: 0;
-            z-index: -1;
+		content: "";
+		position: absolute;
+		top: 0;
+		left: 0;
+		right: 0;
+		bottom: 0;
+		z-index: -1;
 	}
 `;
